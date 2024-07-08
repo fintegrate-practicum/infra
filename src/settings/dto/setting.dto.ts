2a7a6cb46e86e7d43b7bde47a364e89ff83a48f8
ayalale <0527679694a@gmail.com>
2024-07-08 23:21:57 +0300

    FIX

--- src/settings/dto/setting.dto.ts
+++ src/settings/dto/setting.dto.ts
@@ -1,5 +1,6 @@
-import { IsString, IsObject } from "class-validator";
+import { IsString, IsArray, ValidateNested, IsObject } from "class-validator";
 import { Types } from "mongoose";
+import { Type } from "class-transformer";
 
 export class CreateSettingDto {
   @IsString()
@@ -8,3 +9,19 @@ export class CreateSettingDto {
   @IsObject()
   setting_json: Record<string, any>;
 }
+
+export class SettingDto {
+  @IsObject()
+  setting_json: Record<string, any>;
+}
+
+export class SettingServiceDto {
+  @IsString()
+  settingServiceName: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => SettingDto)
+  settings: SettingDto[];
+}
+
