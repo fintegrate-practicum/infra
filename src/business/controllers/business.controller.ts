afbcc27c24fdc33ec2909d7b808a2530f154c136
475michal <166406013+475michalk@users.noreply.github.com>
2024-08-01 01:56:50 +0300

    GC

--- src/business/controllers/business.controller.ts
+++ src/business/controllers/business.controller.ts
@@ -10,32 +10,29 @@ import {
   HttpStatus,
   Query,
   UseGuards,
-} from "@nestjs/common";
-import { BusinessService } from "../services/business.service";
-import { VerificationService } from "../../verification/vertification.services";
-import { CreateBusinessDto } from "../dto/create-busin-first.dto";
-import { CreateBusinessDtoLevel2 } from "../dto/create-busin-secons.dto";
-import * as fs from "fs";
-import { AuthGuard } from "@nestjs/passport";
+} from '@nestjs/common';
+import { BusinessService } from '../services/business.service';
+import { VerificationService } from '../../verification/vertification.services';
+import { CreateBusinessDto } from '../dto/create-busin-first.dto';
+import { CreateBusinessDtoLevel2 } from '../dto/create-busin-secons.dto';
+import * as fs from 'fs';
+import { AuthGuard } from '@nestjs/passport';
 
-@Controller("business")
-@UseGuards(AuthGuard("jwt"))
+@Controller('business')
+@UseGuards(AuthGuard('jwt'))
 export class businessController {
   constructor(
     private readonly businessService: BusinessService,
     private readonly verificationService: VerificationService,
   ) {}
 
-  @Get(":companyNumber")
-  @UseGuards(AuthGuard("jwt"))
-  async getBusinessByCompanyNumber(
-    @Param("companyNumber") companyNumber: string,
-  ) {
+  @Get(':companyNumber')
+  @UseGuards(AuthGuard('jwt'))
+  async getBusinessByCompanyNumber(@Param('companyNumber') companyNumber: string) {
     try {
-      const response =
-        this.businessService.getBusinessByCompanyNumber(companyNumber);
+      const response = this.businessService.getBusinessByCompanyNumber(companyNumber);
       if (!response) {
-        throw new HttpException("business not found", HttpStatus.BAD_REQUEST);
+        throw new HttpException('business not found', HttpStatus.BAD_REQUEST);
       }
       return response;
     } catch (error) {
@@ -43,13 +40,13 @@ export class businessController {
     }
   }
 
-  @Get("link/:linkUID")
-  @UseGuards(AuthGuard("jwt"))
-  async getBusinessByLinkUID(@Param("linkUID") linkUID: string) {
+  @Get('link/:linkUID')
+  @UseGuards(AuthGuard('jwt'))
+  async getBusinessByLinkUID(@Param('linkUID') linkUID: string) {
     try {
       const response = await this.businessService.getBusinessByLinkUID(linkUID);
       if (!response) {
-        throw new HttpException("business not found", HttpStatus.BAD_REQUEST);
+        throw new HttpException('business not found', HttpStatus.BAD_REQUEST);
       }
       return response;
     } catch (error) {
@@ -57,14 +54,13 @@ export class businessController {
     }
   }
 
-  @Delete(":companyNumber")
-  @UseGuards(AuthGuard("jwt"))
-  deleteBusinessByCompanyNumber(@Param("companyNumber") companyNumber: string) {
+  @Delete(':companyNumber')
+  @UseGuards(AuthGuard('jwt'))
+  deleteBusinessByCompanyNumber(@Param('companyNumber') companyNumber: string) {
     try {
-      const response =
-        this.businessService.deleteBusinessByCompanyNumber(companyNumber);
+      const response = this.businessService.deleteBusinessByCompanyNumber(companyNumber);
       if (!response) {
-        throw new HttpException("business not found", HttpStatus.BAD_REQUEST);
+        throw new HttpException('business not found', HttpStatus.BAD_REQUEST);
       }
       return response;
     } catch (error) {
@@ -72,47 +68,49 @@ export class businessController {
     }
   }
 
-  @Post("")
-  @UseGuards(AuthGuard("jwt"))
+  @Post('')
+  @UseGuards(AuthGuard('jwt'))
   async createBusiness(@Body() business: CreateBusinessDto) {
     try {
       const response = this.businessService.createBusiness(business);
       if (!response) {
-        throw new HttpException("business not found", HttpStatus.BAD_REQUEST);
+        throw new HttpException('business not found', HttpStatus.BAD_REQUEST);
       }
       return response;
     } catch (error) {
       throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
-
-  @Put(":companyNumber")
-  @UseGuards(AuthGuard("jwt"))
+  @Put(':companyNumber')
   async updateBusinessByCompanyNumber(
-    @Param("companyNumber") companyNumber: string,
+    @Param('companyNumber') companyNumber: string,
     @Body() newData: CreateBusinessDtoLevel2,
   ): Promise<CreateBusinessDtoLevel2> {
-    console.log(companyNumber);
+    console.log('Update route accessed'); // בדוק אם זה נדפס
+    console.log(`Company Number: ${companyNumber}`);
+    console.log('New Data:', newData);
 
     try {
       if (newData.logo) {
+        console.log('Processing logo'); // הוסף הדפסות לבדוק אם זה עובר
         const filepath = `./logo/company${companyNumber}.png`;
-        fs.writeFileSync(filepath, newData.logo, { encoding: "base64" });
+        fs.writeFileSync(filepath, newData.logo, { encoding: 'base64' });
         newData.logo = filepath;
       }
 
-      const updatedBusiness =
-        this.businessService.updateBusinessByCompanyNumber(
-          companyNumber,
-          newData,
-        );
+      const updatedBusiness = await this.businessService.updateBusinessByCompanyNumber(
+        companyNumber,
+        newData,
+      );
+      console.log('Business updated'); // הוסף הדפסות לבדוק אם זה עובר
+
       if (!updatedBusiness) {
-        throw new HttpException("business not found", HttpStatus.BAD_REQUEST);
-      } else {
-        return updatedBusiness;
+        throw new HttpException('Business not found', HttpStatus.BAD_REQUEST);
       }
+      return updatedBusiness;
     } catch (error) {
-      throw new HttpException(error, HttpStatus.INTERNAL_SERVER_ERROR);
+      console.error('Update error:', error); // הוסף הדפסות לעקוב אחרי בעיות
+      throw new HttpException(error.message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
   }
 }
