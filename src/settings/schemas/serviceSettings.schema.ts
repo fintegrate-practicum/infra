be554621fb86d091fe10f05f9c6ec67b18fcb616
noah-cohen <n0548548106@gmail.com>
2024-08-04 15:27:19 +0200

    removing comments

--- src/settings/schemas/serviceSettings.schema.ts
+++ src/settings/schemas/serviceSettings.schema.ts
@@ -1,26 +1,3 @@
-// import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-// import { Document } from 'mongoose';
-// import { Settings, SettingsSchema } from './settings.schema';
-
-// export type ServiceSettingsDocument = ServiceSettings & Document;
-
-// @Schema()
-// export class ServiceSettings {
-//   @Prop({
-//     type: Number,
-//     required: true,
-//     unique: true,
-//   })
-//   service_id: number;
-
-//   @Prop({
-//     type: String,
-//     required: true,
-//   })
-//   settings_json: Any;
-// }
-
-// export const ServiceSettingsSchema = SchemaFactory.createForClass(ServiceSettings);
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document, Schema as MongooseSchema } from 'mongoose';
 
